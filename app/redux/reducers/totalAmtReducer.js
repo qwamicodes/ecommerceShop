55c48361efd3970540bc45e5dcd1094e3e5d9d38
qwami-vibes <qwaedjekum@gmail.com>
2021-11-17 22:52:45 +0000

    modified the state in redux

--- app/redux/reducers/totalAmtReducer.js
+++ app/redux/reducers/totalAmtReducer.js
@@ -1,9 +1,9 @@
-const initState = { subtotal: 0, tax: 0, total: 0 };
+const initState = { total: 0 };
 
 const totalAmtReducer = (state = initState, action) => {
   switch (action.type) {
     case "UPDATE_TOTAL":
-      return { ...action.payload };
+      return { ...state, ...action.payload };
     case "RESET_TOTAL":
       return { ...initState };
     default:
